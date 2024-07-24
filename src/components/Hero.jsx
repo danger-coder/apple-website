de1490a9b12290acb42b47411ad6aa6e4d416478
karan chaudhary <karanchy95@gmail.com>
2024-07-24 16:51:40 +0545

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,45 @@
-
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { heroVideo,smallHeroVideo } from "../utils";
+import { heroVideo, smallHeroVideo } from "../utils";
 import { useState } from "react";
+import { useEffect } from "react";
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
 
-    const [videoSrc,setVideoSrc]= useState(window.innerWidth <760 ? smallHeroVideo : heroVideo)
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 768) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
 
-    useGSAP(()=>{
-gsap.to('#hero',{opacity: 1,delay:1.5})
-    },[])
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+  }, []);
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-<p id="hero" className="hero-title">iPhone 15 Pro</p>
-<div className="md:w-10/12 w-9/12">
-<video autoPlay muted playsInline={true} key={videoSrc}>
-    <source src={videoSrc} type="video/mp4"/>
-</video>
-
-</div>
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
