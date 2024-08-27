43531d1a6d30593bfc651159d1a7f3cd43a60553
karan chaudhary <karanchy95@gmail.com>
2024-08-27 22:26:02 +0545

    changes

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,28 +1,27 @@
-import { hightlightsSlides } from "../constants"
-
+import { hightlightsSlides } from "../constants";
 
 const VideoCarousel = () => {
   return (
     <>
-    <div className="flex items-center">
+      <div className="flex items-center">
+        {hightlightsSlides?.map((list, i) => (
+          <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl bg-black overflow-hidden">
+                <video id="video" playsInline={true} preload="auto" muted>
+                  <source src={list.video} type="video/mp4"></source>
+                </video>
+                <div className="absolute top-12 left-[5%] z-10">
+                  test
 
-{hightlightsSlides?.map((list,i)=>(
-    <div key={list.id} id="slider" className="sm:pr-20 pr-10">
-        <div className="video-carousel_container">
-<div className="w-full h-full flex-center rounded-3xl bg-black overflow-hidden">
-<video id="video" playsInline={true} preload="auto" muted>
-    <source src={list.video} type="video/mp4" >
-    </source>
-</video>
-</div>
-        </div>
-
-    </div>
-))}
-    </div>
-      
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default VideoCarousel
+export default VideoCarousel;
