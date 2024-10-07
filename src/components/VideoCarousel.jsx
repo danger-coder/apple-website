ce51ddeea72cced569acbad82e5558bac59174ef
karan chaudhary <karanchy95@gmail.com>
2024-10-07 20:38:50 +0545

    cahnges

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -12,7 +12,6 @@ const VideoCarousel = () => {
                   <source src={list.video} type="video/mp4"></source>
                 </video>
                 
-                
                 <div className="absolute top-12 left-[5%] z-10">
                   {list.textLists.map((text)=>(
                     <p key={text.id} className="text-white text-xs font-medium">
